4ee6975c5c5059f94abdd27bcada61e65f1d7e20
Sloane-Z <50128500+Sloane-Z@users.noreply.github.com>
2021-06-26 19:41:45 -0230

    no message

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -3,10 +3,51 @@ import styled, { keyframes, css } from 'styled-components';
 
 import workData from '../content/content';
 
+/***** Keyframes *****/
+const slideUp = keyframes`
+  0%{
+		transform: translateY(100px)
+	}  
+  100%{
+		transform: translateY(0)
+	}
+`
+const slideRight = keyframes`
+  0%{
+		transform: translateX(-200px)
+	}  
+  100%{
+		transform: translateX(0)
+	}
+`
+
+const slideLeft = keyframes`
+  0%{
+		transform: translateX(500px)
+	}  
+  100%{
+		transform: translateX(0)
+	}
+`
+/****** Animation *****/
+
+const slideUpAnim = css`
+	animation: ${slideUp} 0.9s cubic-bezier(0.65, 0, 0.35, 1) both;
+`
+const slideRightAnim = css`
+	animation: ${slideRight} 0.9s cubic-bezier(0.65, 0, 0.35, 1) both;
+`
+const slideLefttAnim = css`
+	animation: ${slideLeft} 1s cubic-bezier(0.65, 0, 0.35, 1) both;
+`
+
 const Wrapper = styled.div`
 	text-align:left;
 	padding-left:10%;
 	padding-right:10%;
+	padding-top: 60px;
+	padding-bottom: 20px; 
+	
 `
 const Header = styled.h1`
 	font-family: 'Montserrat', serif; 
@@ -15,21 +56,21 @@ const Header = styled.h1`
 	font-weight: 400; 
 	font-size: 30px; 
 	display: inline-block;
-
+	${slideRightAnim};
 	::after{
 	content: ''; 
 		display: block; 
 		border-bottom: 1px solid #DFDFDF; 
 		padding-top: 3px;
 	}
-
 `
 
 const SubHeader = styled.h1`
 	font-family: 'Cardo', serif; 
 	font-size: 19px; 
 	line-height: 1.35em; 
-
+  margin-top: 25px;
+	padding-bottom: 10px;
 `
 
 const SubTitle = styled.p`
@@ -42,7 +83,8 @@ const SubTitle = styled.p`
 const Content = styled.p`
   font-family: 'Quattrocento Sans', sans-serif;
 	font-size: 16px;
-	margin-bottom: 25px; 
+	margin:0;
+
 `
 const Responsibility = styled.p`
   font-family: 'Quattrocento Sans', sans-serif;
@@ -51,34 +93,56 @@ const Responsibility = styled.p`
 		content:'{'\u2B24'}',
 	}
 	margin:0;
+	
 `
 
+const ContentWrapper = styled.div`
+	${slideLefttAnim};
+`
+const InfoWrapper = styled.div`
+	margin-bottom: 15px;
+`
 export default class Experience extends Component {
 	render() {
 		return (
+			<>
 			<Wrapper>
 				<Header>Education</Header>
-				<SubHeader>Memorial University of Newfoundland </SubHeader>
-				<Content>Bachelor of Science in Computer Science (2018-current)</Content>
-				<SubHeader>Harbin Institute of Technology</SubHeader>
-				<Content>Bachelor of Science in Finance (2012-2016)</Content>
+				<ContentWrapper>
+					<SubHeader>Memorial University of Newfoundland </SubHeader>
+					<Content>Bachelor of Science in Computer Science (Sep 2018 - current)</Content>
+					<SubHeader>Harbin Institute of Technology</SubHeader>
+					<Content>Bachelor of Economics in Finance (Aug 2012- Jul 2016)</Content>
+				</ContentWrapper>
 
-				<Header>Work</Header>
-				{
-					workData.map((item, index)=>(
-						<>
-						<SubHeader>{item.company}</SubHeader>
-						<Content>{item.title} ({item.timeRange})</Content>
-						<SubTitle>{item.techStack ==='' ? '':(<strong>TechStack: </strong>)}{item.techStack}</SubTitle>
-						{
-							item.responsibility.map((item)=>(
-								<Responsibility>{item}</Responsibility>
-							))
-						}
-						</>
-					))
-				}
 			</Wrapper>
+
+			<Wrapper>
+				<Header>Work</Header>
+				<ContentWrapper>
+					{
+						workData.map((item, index)=>(
+							<>
+							
+							<SubHeader>{item.company}</SubHeader>
+							<InfoWrapper>
+								<Content>{item.title} ({item.timeRange})</Content>
+								<Content>{item.location}</Content>
+								
+							</InfoWrapper>		
+							<SubTitle>{item.techStack ==='' ? '':(<strong>TechStack: </strong>)}{item.techStack}</SubTitle>		
+							{
+								item.responsibility.map((item)=>(
+									<Responsibility>{'\u2022'} {item}</Responsibility>
+								))
+							}
+							</>
+						))
+					}
+				</ContentWrapper>
+
+			</Wrapper>
+			</>
 		)
 	}
 }
--- src/content/content.js
+++ src/content/content.js
@@ -2,8 +2,9 @@ const workData = [
   {
     company:'Beaufort Solutions',
     title:'Junior Software Developer',
-    timeRange:'2020 May - 2021 Sep',
+    timeRange:'May 2020 - Sep 2021',
     techStack: 'Java, C#, Xamarin, Xamarin UI Test, Selenium',
+    location: 'St.John\'s, NL, Canada ',
     responsibility:[
       'Modified unit test for project API according to new changes and release',
       'Debugging applications and implementing code fixes when necessary',
@@ -11,6 +12,19 @@ const workData = [
       'Implemented the data verification on automation testing project using .NET API Microsoft Excel Interop',
       'Developed and executed test plans and test cases for UAT, functional and regression testing',
       'Developed an automation tool for testing accessibilities of website using Selenium and Java'
+    ]
+  },
+
+  {
+    company:'Publishing House of Electronic Industry',
+    title:'Translator',
+    timeRange:'May 2017 - Aug 2017',
+    techStack: '',
+    location:'Remote, China',
+    responsibility:[
+      'Translated publications from English into Chinese',
+      'Collaborated with team to complete client translation projects by deadline',
+      'Publication: (2007). Introduction to Game Design, Prototyping, and Development. Beijing: PHEI'
     ]
   }
 ]
